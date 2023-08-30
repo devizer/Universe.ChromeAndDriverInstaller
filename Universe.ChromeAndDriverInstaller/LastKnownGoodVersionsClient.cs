f1466bb0516b4a8f0e47a7cdf720a978e8e679bc
devizer <devizer@users.github.io>
2023-08-30 14:19:29 +0300

    Brush up LastKnownGoodVersionsClient and tests

--- Universe.ChromeAndDriverInstaller/LastKnownGoodVersionsClient.cs
+++ Universe.ChromeAndDriverInstaller/LastKnownGoodVersionsClient.cs
@@ -37,36 +37,4 @@ namespace Universe.ChromeAndDriverInstaller
             return new ChromeLastKnownGoodVersions(ret);
         }
     }
-
-    public class ChromeLastKnownGoodVersions
-    {
-        // Key: Stable|Beta|Dev|Canary
-        public readonly Dictionary<string, string> Dictionary;
-
-        public ChromeLastKnownGoodVersions(Dictionary<string, string> asDictionary)
-        {
-            Dictionary = asDictionary;
-        }
-
-        public Version TryGetStableVersion() => TryGetVersion("Stable");
-        public Version TryGetBetaVersion() => TryGetVersion("Beta");
-
-        public Version TryGetVersion(string channel)
-        {
-            if (true != Dictionary?.TryGetValue(channel, out var rawVersion)) rawVersion = null;
-            if (rawVersion != null)
-            {
-                try
-                {
-                    return new Version(rawVersion);
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-
-            return null;
-        }
-    }
 }
