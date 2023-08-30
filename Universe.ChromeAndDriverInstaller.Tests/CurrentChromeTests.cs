6a95feddc03ef90b9832c564f2cfe7097e3d687f
devizer <devizer@users.github.io>
2023-08-31 01:48:35 +0300

    MacOS Lab1

--- Universe.ChromeAndDriverInstaller.Tests/CurrentChromeTests.cs
+++ Universe.ChromeAndDriverInstaller.Tests/CurrentChromeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using NUnit.Framework;
 using Universe.NUnitTests;
 
@@ -19,6 +20,17 @@ namespace Universe.ChromeAndDriverInstaller.Tests
             if (!TinyCrossInfo.IsWindows) return;
             Console.WriteLine(CurrentChromeVersionClient.GetWindowsChromeVersion());
         }
+
+        [Test]
+        public void MacOsLab()
+        {
+            if (CrossInfo.ThePlatform != CrossInfo.Platform.MacOSX) return;
+            const string bin = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+            Console.WriteLine($"BIN: {bin}");
+            Console.WriteLine($"BIN EXISTS: {File.Exists(bin)}");
+            Console.WriteLine($"BIN DIR EXISTS: {Directory.Exists(Path.GetDirectoryName(bin))}");
+        }
+
 
         [Test]
         public void TestMajorVersion()
