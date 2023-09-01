c71d43c257a62f900a6ad1580d1289cef3d57ca9
devizer <devizer@users.github.io>
2023-09-01 07:21:15 +0300

    added GetNixChromeVersionRegression

---
+++ Universe.ChromeAndDriverInstaller.Tests/GetNixChromeVersionRegression.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Universe.NUnitTests;
+
+namespace Universe.ChromeAndDriverInstaller.Tests
+{
+    public class GetNixChromeVersionRegression : NUnitTestsBase
+    {
+        [Test]
+        public void Test30Seconds()
+        {
+            if (CrossInfo.ThePlatform != CrossInfo.Platform.Linux) return;
+            Stopwatch sw = Stopwatch.StartNew();
+            int n = 0;
+            while (sw.Elapsed.TotalSeconds < 30)
+            {
+                string chromeVersion = CurrentChromeVersionClient.GetNixChromeVersion();
+                n++;
+                Assert.IsTrue(!string.IsNullOrEmpty(chromeVersion), $"{n}: missing chrome version");
+            }
+        }
+    }
+}
