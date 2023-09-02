215c504c3a29d42418006267e5b5685d73fe8f79
devizer <devizer@users.github.io>
2023-09-02 06:47:58 +0300

    Screenshots on Nix v2

--- Universe.ChromeAndDriverInstaller.Tests/ScreenshotSmartSaver.cs
+++ Universe.ChromeAndDriverInstaller.Tests/ScreenshotSmartSaver.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace Universe.ChromeAndDriverInstaller.Tests
 {
     internal class ScreenshotSmartSaver
     {
+        // Workaround
+        public static void SaveAsPng(Screenshot screenshot, string fileName)
+        {
+            var asArray = screenshot.AsByteArray;
+            var eightBytes = string.Join(" ", asArray.Take(8));
+            if (eightBytes == "137 80 78 71 13 10 26 10")
+            {
+                File.WriteAllBytes(fileName + ".bin", asArray);
+            }
+            else
+            {
+                screenshot.SaveAsFile(fileName, ScreenshotImageFormat.Png);
+            }
+        }
     }
 }
